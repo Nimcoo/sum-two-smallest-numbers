0372a18379068f2e31a64b65f1ef8f4d6fbc2e09
NIMCOO <nimco77001@gmail.com>
2023-12-08 09:28:25 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+//sumTwoSmallestNums([12, 5, 24, 55, 18])
+const numbers = [12, 5, 24, 55, 18];{
+let sum =0;
+return smallestNums = math.max(...numbers);
+}
+ Array.For(n=> sum += n);
+   console.log(sum)
+es
